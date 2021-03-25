b7dca262abf71c70ca319180700f75d7026870ad
artoriaschan <danielovelol@163.com>
2021-03-25 16:07:07 +0800

    feat: 更新deploy脚本

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git add -A
 git commit -m "GitHub Actions Auto Builder at $(date +'%Y-%m-%d %H:%M:%S')"
 
 # push 到 gh-pages 分支
-remote_repo="https://${GITHUB_ACTOR}:${BLOG_DEPLOY_TOKEN}@github.com:artoriaschan/blog.git"
+remote_repo="https://${GITHUB_ACTOR}:${BLOG_DEPLOY_TOKEN}@github.com/artoriaschan/blog.git"
 
 git push --force --quiet "${remote_repo}" "master:gh-pages"
 cd -
